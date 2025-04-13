0b23b523758a10cc4cf9017c27f1bc0a51e0d4d1
kaiba <kaibadash@gmail.com>
2025-04-13 22:27:21 +0900

    Rubocop

--- backend/app/models/metric_value.rb
+++ backend/app/models/metric_value.rb
@@ -7,8 +7,8 @@ class MetricValue < ApplicationRecord
   validates :recorded_at, presence: true
 
   def value_with_unit(prefix)
-    formatted_value = value.to_formatted_s(:delimited)
-    
+    formatted_value = value.to_fs(:delimited)
+
     return "#{metric.unit}#{formatted_value}" if prefix
 
     "#{formatted_value}#{metric.unit}"
